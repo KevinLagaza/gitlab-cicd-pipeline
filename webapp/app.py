d51284f59f24dc834d3b7fd6ac78388f9b80e129
Pirewa Lagaza <blabla@gmail.com>
2026-02-09 23:28:43 +0100

    modifying app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     return 'Hello world!'
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    
+
